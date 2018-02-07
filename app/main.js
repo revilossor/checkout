4769331f57407ffbeab8cdcd10a27f3bd88c721c
Oliver <oliver.ross@hotmail.com>
2018-02-07 19:21:09 +0000

    use api router

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const app = require('express')()
 
+app.use('/api', require('./router/api'))
+
 app.listen(process.env.PORT, () => {
   console.log(`app listening on port ${process.env.PORT}`)
 })
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,15 +1,23 @@
 const dotenv = require('dotenv')
 const express = require('express')
 
+const mockApiRouter = jest.fn()
+
 beforeAll(() => {
   jest.spyOn(dotenv, 'config')
   jest.spyOn(express.application, 'listen').mockImplementation((port, cb) => { cb() })    // dont ACTUALLY listen, so tests dont hang...
+  jest.spyOn(express.application, 'use')
   console.log = jest.fn()
+  jest.mock('../app/router/api', () => mockApiRouter)
   require('../app/main')
 })
 
 it('loads environment from file', () => {
   expect(dotenv.config).toHaveBeenCalled()
+})
+
+it('uses the api router on /api', () => {
+  expect(express.application.use).toHaveBeenCalledWith('/api', mockApiRouter)
 })
 
 it('listens on the process.env.PORT', () => {
